ea791b7708e5ca4bfe150067b4a6aa95de7ad8d5
Nodirbek94 <nodirbekmaksudov@yahoo.com>
2020-09-11 21:03:17 -0400

    finished with the rest of the code

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 
         localStorage.setItem(time, text);
     });
-
+    //saves and loades saved local storage data
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -21,5 +21,31 @@ $(document).ready(function () {
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-    
+
+    function hourTrack() {
+        var currentHour = moment().hour();
+
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            
+            //time check to assign specific classes
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+
+            }
+            if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).addClass("future");
+                $(this).removeClass("present");
+            }
+        })
+    }
+    hourTrack();
 });
